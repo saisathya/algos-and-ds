c1c10e03a61d50122c895085d02665373e090fb9
[Sai Rajandiran] <[saisathyanantha@gmail.com]>
2020-06-14 23:14:40 -0700

    Added sliding window problems

---
+++ src/test/java/sliding_window/ProblemsTest.java
@@ -0,0 +1,12 @@
+package sliding_window;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProblemsTest {
+    @Test
+    public void problem1(){
+        assertEquals(true, true);
+    }
+}
