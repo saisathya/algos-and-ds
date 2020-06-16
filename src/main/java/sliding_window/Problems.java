067194f7ef5019127a794905d3079f2f83f1e910
[Sai Rajandiran] <[saisathyanantha@gmail.com]>
2020-06-15 19:19:47 -0700

    Added 5 sliding window problems.

--- src/main/java/sliding_window/Problems.java
+++ src/main/java/sliding_window/Problems.java
@@ -1,7 +1,121 @@
 package sliding_window;
 
+import java.util.*;
+
 public class Problems {
-    public static void main(String args[]){
-        System.out.println("Hi");
+
+    /**
+     * Find the maximum sum of contiguous subarray of size k
+     */
+    public static int problem1(int[] arr, int k){
+        if(arr == null || arr.length < k || k < 0)
+            return 0;
+
+        int sum = 0, max = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+            if(i >= k){
+                sum -= arr[i - k];
+            }
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+
+    /**
+     *  Find smallest contiguous subarray whose sum is greater than or equal to S
+     */
+    public static int problem2(int[] arr, int S){
+        if(arr == null || arr.length == 0)
+            return 0;
+        int left = 0, sum = 0, min = Integer.MAX_VALUE;
+
+        for(int right = 0; right < arr.length; right++){
+            sum += arr[right];
+
+            while(sum >= S){
+                min = Math.min(min, right - left + 1);
+                sum -= arr[left];
+                left++;
+            }
+        }
+        return min;
+    }
+
+    /**
+     * Find the longest substring with no more than K distinct characters
+     */
+    public static int problem3(String s, int k){
+        if(s == null || s.length() < 0)
+            return 0;
+        Map<Character, Integer> map = new HashMap<>();
+        int max = Integer.MIN_VALUE;
+        int left = 0;
+
+        for(int right = 0; right < s.length(); right++){
+            char c = s.charAt(right);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+
+            while(map.size() > k){
+                char temp = s.charAt(left);
+                map.put(temp, map.get(temp) - 1);
+                if(map.get(temp) == 0)
+                    map.remove(temp);
+                left++;
+            }
+            max = Math.max(max, right - left + 1);
+        }
+
+        return max;
+    }
+
+    /**
+     * Given an array of chars, find the longest contiguous sequence of two unique character
+     */
+    public static int problem4(char[] chars){
+        if(chars == null || chars.length == 0)
+            return 0;
+
+        Map<Character, Integer> map = new HashMap<>();
+        int left = 0, right = 0, max = 0;
+
+        for(; right < chars.length; right++){
+            map.put(chars[right], map.getOrDefault(chars[right], 0) + 1);
+
+            while(map.size() > 2){
+                map.put(chars[left], map.get(chars[left]) - 1);
+                if(map.get(chars[left]) == 0)
+                    map.remove(chars[left]);
+                left++;
+            }
+            max = Math.max(max, right - left + 1);
+        }
+        return max;
+    }
+
+    /**
+     * Given a string, find the longest substring with unique characters
+     */
+    public static int problem5(String s){
+        if(s == null || s.length() == 0)
+            return 0;
+
+        int left = 0, right = 0, max = 0;
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(; right < s.length(); right++){
+            char c = s.charAt(right);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+
+            while(map.size() != (right - left + 1)){
+                c = s.charAt(left);
+                map.put(c, map.get(c) - 1);
+                if(map.get(c) == 0)
+                    map.remove(c);
+                left++;
+            }
+            max = Math.max(max, right - left + 1);
+        }
+        return max;
     }
 }
--- src/test/java/sliding_window/ProblemsTest.java
+++ src/test/java/sliding_window/ProblemsTest.java
@@ -1,12 +1,39 @@
 package sliding_window;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class ProblemsTest {
     @Test
     public void problem1(){
-        assertEquals(true, true);
+        assertEquals(9, Problems.problem1(new int[]{2, 1, 5, 1, 3, 2}, 3));
+        assertEquals(7, Problems.problem1(new int[]{2, 3, 4, 1, 5}, 2));
+    }
+
+    @Test
+    public void problem2(){
+        assertEquals(2, Problems.problem2(new int[]{2, 1, 5, 2, 3, 2}, 7));
+        assertEquals(1, Problems.problem2(new int[]{2, 3, 5, 2, 8}, 7));
+        assertEquals( 3, Problems.problem2(new int[]{3, 4, 1, 1, 6}, 8));
+    }
+
+    @Test
+    public void problem3(){
+        assertEquals(4, Problems.problem3("araaci", 2));
+        assertEquals(2, Problems.problem3("araaci", 1));
+        assertEquals(5, Problems.problem3("cbbebi", 3));
+    }
+
+    @Test
+    public void problem4(){
+        assertEquals(3, Problems.problem4(new char[]{'A', 'B', 'C', 'A', 'C'}));
+        assertEquals(5, Problems.problem4(new char[]{'A', 'B', 'C', 'B', 'B', 'C'}));
+    }
+
+    @Test
+    public void problem5(){
+        assertEquals(3, Problems.problem5("aabccbb"));
+        assertEquals(2, Problems.problem5("abbbb"));
+        assertEquals(3, Problems.problem5("abccde"));
     }
 }
