4e083f2091f7575b1ed100a02e4c1e5132608cd2
[Sai Rajandiran] <[saisathyanantha@gmail.com]>
2020-07-16 06:16:19 -0700

    Added questions for cyclic_sort.

--- src/main/java/cyclic_sort/Problems.java
+++ src/main/java/cyclic_sort/Problems.java
@@ -1,4 +1,54 @@
 package cyclic_sort;
 
 public class Problems {
+    /**
+     * Given an array containing objects 1...n, n being the size of the array. Sort it in place.
+     */
+    public static int[] problem1(int[] arr){
+        if(arr == null || arr.length ==  0)
+            return arr;
+
+        int i = 0;
+        while(i < arr.length){
+            int temp = arr[i] - 1;
+            if(arr[i] == arr[temp])
+                i++;
+            else{
+                swap(arr, i, arr[i] - 1);
+            }
+        }
+        return arr;
+    }
+
+    /**
+     * Given an array size n, it has distinct values from 0...n. Find the missing integer
+     */
+    public static int problem2(int[] arr){
+        if(arr == null)
+            return -1;
+        int idx = -1;
+        int i = 0;
+        while(i < arr.length){
+            if(arr[i] == arr.length){
+                idx = i;
+                i++;
+            }
+            else if(arr[i] == i)
+                i++;
+            else
+                swap(arr, i, arr[i]);
+        }
+        return (idx == -1)? arr.length : idx;
+    }
+
+    /**
+     * Given an array containing values from 1...n, n being the size. Find the missing values if it may contain
+     * duplicates
+     */
+
+    private static void swap(int[] a, int i , int j){
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
 }
--- src/test/java/cyclic_sort/ProblemsTest.java
+++ src/test/java/cyclic_sort/ProblemsTest.java
@@ -1,7 +1,20 @@
 package cyclic_sort;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ProblemsTest {
+    @Test
+    public void problem1(){
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, Problems.problem1(new int[] { 3, 1, 5, 4, 2 }));
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6}, Problems.problem1(new int[] { 2, 6, 4, 3, 1, 5 }));
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6}, Problems.problem1(new int[]{1, 5, 6, 4, 3, 2}));
+    }
 
+    @Test
+    public void problem2(){
+        assertEquals(2, Problems.problem2(new int[]{4, 0, 3, 1}));
+        assertEquals(7, Problems.problem2(new int[]{8, 3, 5, 2, 4, 6, 0, 1}));
+    }
 }
