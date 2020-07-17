717ed54cc5f78053076d11d0afbc8a053e978ff4
Sathya Rajandiran <saisathyanantha@gmail.com>
2020-07-16 18:57:34 -0700

    New problems in cyclic_sort.

--- src/main/java/cyclic_sort/Problems.java
+++ src/main/java/cyclic_sort/Problems.java
@@ -1,5 +1,7 @@
 package cyclic_sort;
 
+import java.util.*;
+
 public class Problems {
     /**
      * Given an array containing objects 1...n, n being the size of the array. Sort it in place.
@@ -45,10 +47,64 @@ public class Problems {
      * Given an array containing values from 1...n, n being the size. Find the missing values if it may contain
      * duplicates
      */
+    public static List<Integer> problem3(int[] arr){
+        if(arr == null || arr.length == 0)
+            return new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        int i = 0;
+        while(i < arr.length){
+            if(arr[i] - 1 == i || arr[arr[i] - 1] == arr[i])
+                i++;
+            else
+                swap(arr, i, arr[i] - 1);
+        }
+        for(int j = 0; j < arr.length; j++){
+            if(arr[j] - 1 != j)
+                result.add(j + 1);
+        }
+        return result;
+    }
+
+    /**
+     * Given an array containing values from 1 to n, with n + 1 being the size of the array
+     * Find the duplicate, only one value is duplicated
+     */
+
+    public static int problem4(int[] arr){
+        if(arr == null || arr.length == 0)
+            return -1;
+        int i = 0;
+        while(i < arr.length){
+            if(arr[i] - 1 == i){
+                i++;
+            }
+            else if(arr[arr[i] - 1] == arr[i])
+                return arr[i];
+            else
+                swap(arr, arr[i] - 1, i);
+        }
+
+        return -1;
+    }
+
+    /**
+     * Given an array of size n and values ranging from 1...n, find all duplicates
+     */
+    public static List<Integer> problem5(int[] arr){
+        if(arr == null || arr.length == 0)
+            return new ArrayList<>();
+        List<Integer> output = new ArrayList<>();
+        int i = 0;
+        while(i < arr.length){
+            if(arr[i] - 1 == i)
+                i++;
+            
+        }
+    }
 
     private static void swap(int[] a, int i , int j){
         int temp = a[i];
         a[i] = a[j];
         a[j] = temp;
-    }//some changes
+    }
 }
--- src/test/java/cyclic_sort/ProblemsTest.java
+++ src/test/java/cyclic_sort/ProblemsTest.java
@@ -1,8 +1,9 @@
 package cyclic_sort;
 
 import org.junit.Test;
-
+import java.util.*;
 import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
 
 public class ProblemsTest {
     @Test
@@ -16,5 +17,20 @@ public class ProblemsTest {
     public void problem2(){
         assertEquals(2, Problems.problem2(new int[]{4, 0, 3, 1}));
         assertEquals(7, Problems.problem2(new int[]{8, 3, 5, 2, 4, 6, 0, 1}));
+    }
+
+    @Test
+    public void problem3(){
+        assertThat(Problems.problem3(new int[]{1, 4, 2, 2, 4}), containsInAnyOrder(3, 5));
+        assertThat(Problems.problem3(new int[]{2, 3, 1, 8, 2, 3, 5, 1}), containsInAnyOrder(4, 6, 7));
+        assertThat(Problems.problem3(new int[]{2, 4, 1, 2}), containsInAnyOrder(3));
+        assertThat(Problems.problem3(new int[]{2, 3, 2, 1}), containsInAnyOrder(4));
+    }
+
+    @Test
+    public void problem4(){
+        assertEquals(4, Problems.problem4(new int[]{1, 4, 4, 3, 2}));
+        assertEquals(3, Problems.problem4(new int[]{2, 1, 3, 3, 5, 4}));
+        assertEquals(4, Problems.problem4(new int[]{2, 4, 1, 4, 4}));
     }
 }
