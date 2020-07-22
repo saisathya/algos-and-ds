eea5e9a33b7fd14b645ced1e5863d0171752a95a
Sathya Rajandiran <saisathyanantha@gmail.com>
2020-07-21 19:01:22 -0700

    Added new test for bfs.

--- src/test/java/bfs/ProblemsTest.java
+++ src/test/java/bfs/ProblemsTest.java
@@ -54,4 +54,16 @@ public class ProblemsTest {
         root.right.right = new TreeNode(5);
         assertThat(Problems.problem4(root), hasItems(12.0, 4.0, 6.5));
     }
+
+    @Test
+    public void problem5(){
+        TreeNode root = new TreeNode(12);
+        root.left = new TreeNode(7);
+        root.right = new TreeNode(1);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(5);
+        root.left.left = new TreeNode(9);
+        root.right.left.left = new TreeNode(11);
+        assertEquals(3, Problems.problem5(root));
+    }
 }
