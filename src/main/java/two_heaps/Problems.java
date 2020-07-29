56b78a8f666a08f4dec3bff35409968018dccb4b
Sathya Rajandiran <saisathyanantha@gmail.com>
2020-07-28 17:58:22 -0700

    added problem to two heaps

--- src/main/java/two_heaps/Problems.java
+++ src/main/java/two_heaps/Problems.java
@@ -3,4 +3,28 @@ package two_heaps;
 import java.util.*;
 
 public class Problems {
+    /**
+     * Design a class that helps find the median of an input stream
+     */
+    public static class Problem1{
+        PriorityQueue<Integer> left = new PriorityQueue<>();
+        PriorityQueue<Integer> right = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+
+        public void insert(int num){
+            if(left.isEmpty() && right.isEmpty())
+                right.offer(num);
+            else if(num >= right.peek() || left.size() == right.size()){
+                right.offer(num);
+                left.offer(right.poll());
+            }
+            else
+                left.offer(num);
+        }
+
+        public double findMedian(){
+            if(!left.isEmpty() && left.size() == right.size())
+                return (left.peek() + right.peek()) * 1.0 / 2;
+            return right.peek();
+        }
+    }
 }
--- src/test/java/two_heaps/ProblemsTest.java
+++ src/test/java/two_heaps/ProblemsTest.java
@@ -6,5 +6,11 @@ import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
 public class ProblemsTest {
+    @Test
+    public void problem1(){
+        Problems.Problem1 p1 = new Problems.Problem1();
+        p1.insert(1);
+        assertEquals(1, p1.findMedian());
 
+    }
 }
