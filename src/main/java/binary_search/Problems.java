7673438ffdc56d8b0be273a340b65c1c6676e354
Sathya Rajandiran <saisathyanantha@gmail.com>
2020-08-11 21:59:46 -0700

    added problem to binary_search

--- src/main/java/binary_search/Problems.java
+++ src/main/java/binary_search/Problems.java
@@ -57,4 +57,25 @@ public class Problems {
         }
         return -1;
     }
+
+    /**
+     * Given a sorted list of unique, get the ceiling of a target, which can be either itself or the smallest number greater than the target
+     */
+    public static int problem2(int[] arr, int k){
+        if(arr == null || arr.length == 0)
+            return -1;
+        int left = 0, right = arr.length - 1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(arr[mid] == k)
+                return k;
+            if(k > arr[mid])
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        if(left < arr.length)
+            return arr[left];
+        return -1;
+    }
 }
--- src/test/java/binary_search/ProblemsTest.java
+++ src/test/java/binary_search/ProblemsTest.java
@@ -28,4 +28,22 @@ public class ProblemsTest {
         assertEquals(-1, problem1_alt(new int[]{4, 6, 10}, 3));
         assertEquals(-1, problem1_alt(new int[]{4, 6, 10}, 5));
     }
+
+    @Test
+    public void testProblem2(){
+        int[] arr = new int[]{1, 3, 5, 7, 7, 7, 7, 9};
+        assertEquals(1, problem2(arr, -1));
+        assertEquals(1, problem2(arr, 1));
+        assertEquals(3, problem2(arr, 3));
+        assertEquals(7, problem2(arr, 6));
+        assertEquals(9, problem2(arr, 9));
+        assertEquals(9, problem2(arr, 8));
+        assertEquals(5, problem2(arr, 4));
+        assertEquals(-1, problem2(arr, 10));
+
+        assertEquals(6, problem2(new int[] { 4, 6, 10 }, 6));
+        assertEquals(15, problem2(new int[] { 1, 3, 8, 10, 15 }, 12));
+        assertEquals(-1, problem2(new int[] { 4, 6, 10 }, 17));
+        assertEquals(4, problem2(new int[] { 4, 6, 10 }, -1));
+    }
 }
