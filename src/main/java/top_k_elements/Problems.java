e3e97ef31c9e3ef85996e3a9289bdd612dc2ea97
Sathya Rajandiran <saisathyanantha@gmail.com>
2020-08-17 08:12:12 -0700

    Added problem to top_k_elements

--- src/main/java/top_k_elements/Problems.java
+++ src/main/java/top_k_elements/Problems.java
@@ -141,28 +141,25 @@ public class Problems {
      * Design a class that takes in a stream of numbers and returns the K-th largest value
      */
     public static class Problem7{
-        PriorityQueue<Integer> leftHalf, rightHalf;
+        PriorityQueue<Integer> minHeap;
         int k;
 
         public Problem7(int[] arr, int k){
             this.k = k;
-            leftHalf = new PriorityQueue<>(Comparator.reverseOrder());
-            rightHalf = new PriorityQueue<>();
+            minHeap = new PriorityQueue<>();
 
             for(int i = 0; i < arr.length; i++)
                 add(arr[i]);
         }
 
         public int add(int val){
-            if(rightHalf.size() <= k || val > rightHalf.peek())
-                rightHalf.offer(val);
-            else
-                leftHalf.offer(val);
+            if(minHeap.size() <= k || val > minHeap.peek())
+                minHeap.offer(val);
 
-            if(rightHalf.size() > k)
-                leftHalf.offer(rightHalf.poll());
+            if(minHeap.size() > k)
+                minHeap.poll();
 
-            return rightHalf.peek();
+            return minHeap.peek();
         }
     }
 }
