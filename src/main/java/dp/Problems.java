b7ea917b86757eb97f5a3c2bdd9e16db2b2ff7f4
Sathya Rajandiran <saisathyanantha@gmail.com>
2020-08-20 13:11:23 -0700

    added sample of knapsack recures in dp

--- src/main/java/dp/Problems.java
+++ src/main/java/dp/Problems.java
@@ -1,4 +1,27 @@
 package dp;
 
 public class Problems {
+    /**
+     * template for the most primitive version of 0/1 knapsack
+     * Given a list of item's weight and their profits, find the maximum profit given that you can take
+     * items with the maximum weight of c
+     */
+    public static int knapsack1(int[] weights, int[] profits, int capacity){
+        return knapsack1_recurse(weights, profits, capacity, 0, 0);
+    }
+
+    public static int knapsack1_recurse(int[] weights, int[] profits, int capacity, int profit, int index){
+        if(index == weights.length)
+            return profit;
+
+         int a = knapsack1_recurse(weights, profits, capacity, profit, index + 1);
+         int b = Integer.MIN_VALUE;
+         if(weights[index] <= capacity)
+             b = knapsack1_recurse(weights, profits, capacity - weights[index], profit + profits[index], index + 1);
+         return Math.max(a, b);
+    }
+
+    public static int memoized_knapsack(int[] weights, int[] profits, int capacity){
+
+    }
 }
--- src/test/java/dp/ProblemsTest.java
+++ src/test/java/dp/ProblemsTest.java
@@ -7,5 +7,11 @@ import static dp.Problems.*;
 import java.util.*;
 
 public class ProblemsTest {
-
+    @Test
+    public void knapsack(){
+        int[] weights = new int[]{1, 2, 3, 5};
+        int[] profits = new int[]{1, 6, 10, 16};
+        int capacity = 7;
+        assertEquals(22, knapsack1(weights, profits, capacity));
+    }
 }
