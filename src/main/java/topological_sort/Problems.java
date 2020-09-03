86240be07654103a9d697511da30bc07cf6c149f
Sathya Rajandiran <saisathyanantha@gmail.com>
2020-09-02 20:15:07 -0700

    added solution to topological_sort.problem1

--- src/main/java/topological_sort/Problems.java
+++ src/main/java/topological_sort/Problems.java
@@ -1,4 +1,47 @@
 package topological_sort;
 
+import java.util.*;
+
 public class Problems {
+    /**
+     * Given a directed graph with N nodes, and a list of edges going from U -> V,
+     * find the topological ordering
+     */
+    public static int[] problem1(int n, int[][] edges){
+        if(n <= 0 || edges == null)
+            throw new IllegalArgumentException();
+
+        Map<Integer, Set<Integer>> adjList = new HashMap<>();
+        Map<Integer, Integer> inDegree = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            adjList.put(i, new HashSet<>());
+            inDegree.put(i, 0);
+        }
+
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+
+        for(int[] edge : edges){
+            adjList.get(edge[0]).add(edge[1]);
+            inDegree.put(edge[1], inDegree.get(edge[1]) + 1);
+        }
+
+        for(Map.Entry<Integer, Integer> entry : inDegree.entrySet()){
+            if(entry.getValue() == 0)
+                q.offer(entry.getKey());
+        }
+
+        int[] output = new int[n];
+        int idx = 0;
+        while(!q.isEmpty()){
+            int i = q.poll();
+            output[idx++] = i;
+            Set<Integer> neighbors = adjList.get(i);
+            for(int j : neighbors){
+                inDegree.put(j, inDegree.get(j) - 1);
+                if(inDegree.get(j) == 0)
+                    q.offer(j);
+            }
+        }
+        return output;
+    }
 }
--- src/test/java/topological_sort/ProblemsTest.java
+++ src/test/java/topological_sort/ProblemsTest.java
@@ -1,7 +1,22 @@
 package topological_sort;
 
+import org.junit.Test;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
+import static topological_sort.Problems.*;
+import java.util.*;
 
 public class ProblemsTest {
+    @Test
+    public void testProblem1(){
+        assertArrayEquals(array(3, 2, 0, 1), problem1(4, new int[][]{array(3, 2), array(3, 0), array(2, 0), array(2, 1)}));
+        assertArrayEquals(array(4, 2, 0, 3, 1), problem1(5, new int[][]{array(4, 2), array(4, 3), array(2, 0), array(2, 1), array(3, 1)}));
+        assertArrayEquals(array(5, 3, 0, 6, 2, 4, 1),
+                problem1(7, new int[][]{array(6, 4), array(6, 2), array(5, 3), array(5, 4), array(3, 0), array(3, 1), array(3, 2),
+                        array(4, 1)}));
+    }
 
+    static int[] array(int ...a){
+       return a;
+    }
 }
